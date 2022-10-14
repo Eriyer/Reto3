7bc799d5b234827b2e6909eba737505fb5083a09
Eriyer <eriyer2010@gmail.com>
2022-10-13 21:37:52 -0500

    comentario

--- src/main/java/com/example/Reto3/entities/Reservation.java
+++ src/main/java/com/example/Reto3/entities/Reservation.java
@@ -1,10 +1,11 @@
 package com.example.Reto3.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,8 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "Reservation")
@@ -24,25 +24,69 @@ public class Reservation implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "startdate")
-    private String startdate;
+    @Column(name = "startDate")
+    private Date startDate;
 
-    @Column(name = "devolutiondate")
-    private String devolutiondate;
+    @Column(name = "devolutionDate")
+    private Date devolutionDate;
+
+    @Column(name = "status")
+    private String status;
 
     // RELACIONES//
     // Relacion muchos a uno. El message tiene enlazado un Client y un Farm
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "Cliente_id")
-    @JsonProperty(access = Access.WRITE_ONLY)
-    private Client client;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "Farm_id")
-    @JsonProperty(access = Access.WRITE_ONLY)
+    @ManyToOne(optional = false)
+    @JsonIgnoreProperties(value = { "reservations" })
+    @JoinColumn(name = "farm_id")
     private Farm farm;
 
+    @ManyToOne(optional = false)
+    @JsonIgnoreProperties(value = { "messages", "reservations" })
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Column(name = "score")
+    private String score;
+
+    // ***** CONSTRUCTOR *****
+    public Reservation() {
+        this.status = "created";
+        this.score = null;
+    }
+
     // METODOS//
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getDevolutionDate() {
+        return devolutionDate;
+    }
+
+    public void setDevolutionDate(Date devolutionDate) {
+        this.devolutionDate = devolutionDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Farm getFarm() {
+        return farm;
+    }
+
+    public void setFarm(Farm farm) {
+        this.farm = farm;
+    }
 
     public Integer getId() {
         return id;
@@ -52,34 +96,20 @@ public class Reservation implements Serializable {
         this.id = id;
     }
 
-    public String getStartdate() {
-        return startdate;
+    public String getStatus() {
+        return status;
     }
 
-    public void setStartdate(String startdate) {
-        this.startdate = startdate;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public String getDevolutiondate() {
-        return devolutiondate;
+    public String getScore() {
+        return score;
     }
 
-    public void setDevolutiondate(String devolutiondate) {
-        this.devolutiondate = devolutiondate;
-    }
-
-    public Reservation crearReservation(Reservation reservation) {
-        return null;
-    }
-
-    public void eliminarReservation(int id2) {
-    }
-
-    public void actualizarReservation(Integer id2, Reservation reservation) {
-    }
-
-    public Reservation getReservation(int id2) {
-        return null;
+    public void setScore(String score) {
+        this.score = score;
     }
 
 }
